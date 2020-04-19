d50d6ad0e667177ca7de00bd2ee46cdf9a115e63
Federico Castro <fede.castrog@gmail.com>
2020-04-19 16:30:42 -0300

    Add dynamic redirect

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,19 +1,13 @@
 import React, { Fragment, useState } from "react";
 import styles from "./styles";
 import { ScaleLoader } from "react-spinners";
-import querystring from "querystring";
 import Layout from "../../components/Layout";
+import redirect from "../../utils/redirect";
 
 const LoginButton = () => {
   const [loading, setLoading] = useState(false);
 
   const handleClick = () => setLoading(true);
-
-  const param = querystring.stringify({
-    returnTo: "http://localhost:3000/home",
-  });
-
-  const redirect = `https://spotify-project-backend.herokuapp.com/login?${param}`;
 
   return (
     <Fragment>
