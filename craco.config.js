c883bb594ea918587d23202c87ebbbdb97603a5e
Federico Castro <fede.castrog@gmail.com>
2020-05-02 11:04:44 -0300

    Add craco config

---
+++ craco.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+  webpack: {
+    configure: {
+      resolve: {
+        modules: [path.resolve(__dirname, "src"), "node_modules"],
+        alias: { material: "@material-ui/core" },
+      },
+    },
+  },
+};
