e6cd84454ade896a4bd66d17622bb8bfae5cefe2
Federico Castro <fede.castrog@gmail.com>
2020-05-03 13:43:09 -0300

    Add sticky behaviour to bar

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,15 +9,17 @@ import Chart from "./components/Chart";
 
 import { Store } from "context/Store";
 import useFetchData from "hooks/useFetchData";
+import useSticky from "hooks/useSticky";
 
 const Home = () => {
   const [tab, setTab] = useState(0);
   const { store, dispatch } = useContext(Store);
   const { isLoading } = useFetchData();
-  const { type, artists, tracks } = store;
+  const { sticky } = useSticky(84);
 
   const handleType = () => dispatch({ type: "TYPE" });
 
+  const { type, artists, tracks } = store;
   const isArtists = type === "artists";
   const title = `Top ${isArtists ? "Artists" : "Tracks"}`;
   const data = isArtists ? artists : tracks;
@@ -26,11 +28,13 @@ const Home = () => {
     <Layout>
       <Container>
         <styles.Main>
-          <styles.Title>{title}</styles.Title>
-          <styles.RowWrapper>
-            <RangeSelector tab={tab} setTab={setTab} />
-            <TypeSelector handleType={handleType} type={type} />
-          </styles.RowWrapper>
+          <styles.Header sticky={sticky}>
+            <styles.Title sticky={sticky}>{title}</styles.Title>
+            <styles.RowWrapper>
+              <RangeSelector tab={tab} setTab={setTab} />
+              <TypeSelector handleType={handleType} type={type} />
+            </styles.RowWrapper>
+          </styles.Header>
           <Chart {...{ data, isArtists }} range={tab} />
         </styles.Main>
       </Container>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,22 +1,59 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import Typography from "@material-ui/core/Typography";
+import shadows from "theme/shadows";
 
 const Main = styled.main`
   display: grid;
   width: min(90vw, 1280px);
   margin: 40px 0;
-  grid-template-rows: 55px auto auto;
+  grid-template-rows: auto auto;
   gap: 30px;
+`;
+
+const Header = styled.div`
+  display: grid;
+  position: sticky;
+  top: 0;
+  z-index: 30;
+  grid-template-rows: auto auto;
+  gap: 30px;
+  background: #fff;
+  transition: all 0.15s;
+  clip-path: inset(0px 1px -40px 1px);
+  box-shadow: ${shadows.fuzzy};
+
+  ${({ sticky }) =>
+    sticky &&
+    css`
+      padding-top: 25px;
+      gap: 25px;
+    `}
 `;
 
 const Title = styled(Typography).attrs({ variant: "h1" })`
   color: var(--gray-800);
+  background: #fff;
+  transition: all 0.15s;
+  padding-left: 1px;
+  && {
+    line-height: 55px;
+  }
+
+  ${({ sticky }) =>
+    sticky &&
+    css`
+      && {
+        font-size: 22px;
+        line-height: 32px;
+      }
+    `}
 `;
 
 const RowWrapper = styled.div`
   display: flex;
   justify-content: space-between;
-  border-bottom: 1px solid var(--gray-300);
+  /* border-bottom: 1px solid var(--gray-300); */
+  background: #fff;
 `;
 
-export default { Main, Title, RowWrapper };
+export default { Main, Title, RowWrapper, Header };
