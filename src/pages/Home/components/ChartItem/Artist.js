aeb4fcd348d2b0963da645ce8b42a75805c2d6ac
Federico Castro <fede.castrog@gmail.com>
2020-05-24 22:10:19 -0300

    Add lazyloading to image

--- src/pages/Home/components/ChartItem/Artist.js
+++ src/pages/Home/components/ChartItem/Artist.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import { arrayOf, object, string, number, bool } from "prop-types";
 import styles from "./styles";
+import Lazy from "react-lazyload";
 
 import { isMobile } from "react-device-detect";
 import useDevices from "hooks/useDevices";
@@ -22,7 +23,9 @@ const Artist = ({ name, genres, images, rank, isLoading }) => {
       ) : (
         <Fragment>
           <styles.Rank>{rank}</styles.Rank>
-          <styles.Image src={src} alt={name} />
+          <Lazy once>
+            <styles.Image src={src} alt={name} />
+          </Lazy>
           <styles.Name>{name}</styles.Name>
           <styles.Genres>{_genres}</styles.Genres>
         </Fragment>
