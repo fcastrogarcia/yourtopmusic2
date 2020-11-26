5c704e5b71c8e79ceef5a1d7b675d8b4d5c0dc1c
Federico Castro García <fede.castrog@gmail.com>
2020-11-25 22:53:13 -0300

    Refactor home route name

--- lambda/config.js
+++ lambda/config.js
@@ -1,7 +1,7 @@
 const { BASE_URL, CLIENT_ID, CLIENT_SECRET } = process.env;
 const SPOTIFY_URL = "https://accounts.spotify.com";
 const CALLBACK_URL = `${BASE_URL}/.netlify/functions/callback`;
-const HOME_URL = `${BASE_URL}/home`;
+const HOME_URL = `${BASE_URL}/chart`;
 
 module.exports = {
   CALLBACK_URL,
--- src/pages/Home/hooks/useQuery.js
+++ src/pages/Home/hooks/useQuery.js
@@ -14,7 +14,7 @@ const useQuery = () => {
 
   const setQueryParams = search => {
     return history.push({
-      pathname: "/home",
+      pathname: "/chart",
       search,
     });
   };
